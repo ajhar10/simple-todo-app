89e5eb34de35f5243c8d4a9fe7e4f481feb63431
ajhar10 <ajharrimon90@gmail.com>
2022-08-22 15:29:55 +0600

    first commit

---
+++ src/redux/todos/actions.js
@@ -0,0 +1,40 @@
+import {ADDED, TOGGLED, CLEARCOMPLETED, COLORSELECTED, ALLCOMPLETED, DELETED} from './actionTypes'
+
+export const added =(todoText)=>{
+    return{
+    type: ADDED,
+    payload: todoText
+    }    
+}
+export const toggled =(todoId)=>{
+    return{
+    type: TOGGLED,
+    payload: todoId
+    }    
+}
+export const colorselected =(todoId, color)=>{
+    return{
+    type: COLORSELECTED,
+    payload: {
+        todoId,
+        color
+    }
+    }    
+}
+export const deleted =(todoId)=>{
+    return{
+    type: DELETED,
+    payload: todoId
+    }    
+}
+export const allcompleted =()=>{
+    return{
+    type: ALLCOMPLETED
+    }    
+}
+export const clearcompleted =()=>{
+    return{
+    type: CLEARCOMPLETED,
+    
+    }    
+}
